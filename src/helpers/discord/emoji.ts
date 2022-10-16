f88f615f6b794ae9a75f4d1fd3105aaf0b73a739
VTGare <tatarinvlad1@gmail.com>
2022-10-16 18:38:23 +0300

    change emojis

--- src/helpers/discord/emoji.ts
+++ src/helpers/discord/emoji.ts
@@ -1,14 +1,14 @@
 export const emoji: Record<Name, EmojiCode> = {
-  respond: '<:LunaRespond:814173011313295370>',
-  deepl: '<:deepl:835081601003290635>',
-  nbsp: '<:nbsp:832910690998026260>',
-  discord: '<:Discord:832902118301106216>',
-  holo: '<:Hololive:832638929919803422>',
-  ping: '<:WatamePing:864533105821220894>',
-  tc: '<:TwitCasting:832638929608900689>',
-  yt: '<:YouTube:832638929802493962>',
-  peek: '<:LunaPeek:873613928867975248>',
-  niji: '<:nijisanji:893782660156112986>',
+  respond: '<:LunaRespond:1030947788038623282>',
+  deepl: '<:deepl:1030947783907229696>',
+  nbsp: '<:nbsp:1030947789435314198>',
+  discord: '<:Discord:1030947785261985842>',
+  holo: '<:Hololive:1030947786448982087>',
+  ping: '<:WatamePing:1030947793348612126>',
+  tc: '<:TwitCasting:1030947792081924177>',
+  yt: '<:YouTube:1030947794615283732>',
+  peek: '<:GatorYay:977005487012859964>',
+  niji: '<:nijisanji:1030947790769115198>',
 } as const
 
 ///////////////////////////////////////////////////////////////////////////////
